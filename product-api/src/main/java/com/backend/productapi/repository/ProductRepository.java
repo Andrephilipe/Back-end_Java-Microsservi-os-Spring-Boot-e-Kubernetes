c689cc86b587347d713f36360f180fb1528f60c6
André Cassiano <andre.p.cassiano@accenture.com>
2022-06-28 08:07:48 -0300

    iniciando as classes repository

---
+++ product-api/src/main/java/com/backend/productapi/repository/ProductRepository.java
@@ -0,0 +1,24 @@
+package com.backend.productapi.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.backend.productapi.model.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+    
+    @Query(value = "select p "
+            + "from product p "
+            + "join category c on p.category.id = c.id "
+            + "where c.id = :categoryId ")
+
+    public List<Product> getProductByCategory(@Param("categoryId") Long categoryId);
+
+    public Product findByProductIdentifier(String productIdentifier);
+    
+}
