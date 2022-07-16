e774017a6eda19c3cdd9166313ad1c894a3b1072
André Cassiano <andre.p.cassiano@accenture.com>
2022-07-16 08:55:14 -0300

    c

--- Teste.java
+++ Teste.java
@@ -1,1 +1,13 @@
+package com.livro;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Teste {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
